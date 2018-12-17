bdbe0880cc8732d6fc618da9dfada3f5e2267668
Christos Panagiotakopoulos <chrispanag@gmail.com>
2018-12-17 16:48:21 +0200

    Add bot actions

--- examples/hodor-bot/modules/hodor/actions.js
+++ examples/hodor-bot/modules/hodor/actions.js
@@ -6,7 +6,19 @@ function hodor(id, user) {
     const message = new Message({
         text: "Hodor?"
     });
-    return bot.send(id, message);
+    const message2 = new Message({
+        text: "Hodor Tester?"
+    });
+    console.log("test");
+    return bot.scenario(id)
+        .types()
+        .wait(1000)
+        .send(message)
+        .wait(2000)
+        .types()
+        .wait(1000)
+        .send(message2)
+        .end();
 }
 
 module.exports = {
