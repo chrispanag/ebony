4d646c2257b225a800670174f90da4d53a3a39af
Christos Panagiotakopoulos <chrispanag@gmail.com>
2019-09-23 17:42:25 +0300

    add middlewares on actions

--- lib/utilities/actions.ts
+++ lib/utilities/actions.ts
@@ -46,8 +46,8 @@ export default class Actions {
 
     public addMiddlewares(type: 'pre' | 'post', middlewares: ActionMiddleware[]) {
         switch (type) {
-            case 'pre': this.preMiddlewares.concat(middlewares); console.log(this.preMiddlewares); return;
-            case 'post': this.postMiddlewares.concat(middlewares); return;
+            case 'pre': this.preMiddlewares = this.preMiddlewares.concat(middlewares); return;
+            case 'post': this.preMiddlewares = this.postMiddlewares.concat(middlewares); return;
         }
     }
 
