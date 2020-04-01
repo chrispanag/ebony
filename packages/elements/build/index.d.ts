214d109fc6d81775b73b95d4ff2ffa6dc07b41c0
Christos Panagiotakopoulos <chrispanag@gmail.com>
2020-04-01 15:39:07 +0300

    Separate elements from adapter

--- packages/elements/build/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './sendAPI';
-//# sourceMappingURL=index.d.ts.map
--- packages/elements/build/sendAPI/interfaces.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isObjectPayload(payload) {
-    return typeof payload === 'object';
-}
-exports.isObjectPayload = isObjectPayload;
-//# sourceMappingURL=interfaces.js.map
