c33d30f39b96d651ca843df3079c1c1ec9d8c3f9
Christos Panagiotakopoulos <chrispanag@gmail.com>
2020-12-22 02:40:47 +0200

    [Fix] Fix build

--- examples/cli-bot/src/modules/getStarted/middlewares.ts
+++ examples/cli-bot/src/modules/getStarted/middlewares.ts
@@ -1,5 +1,4 @@
 import { ActionMiddleware, InMemoryUser } from '@ebenos/framework';
-import { MessengerUser } from '@ebenos/messenger-adapter';
 
 export const preMiddlewares: Array<ActionMiddleware<InMemoryUser>> = [
     (actionName: string, user: InMemoryUser, params: any[], next: () => void): void => {
