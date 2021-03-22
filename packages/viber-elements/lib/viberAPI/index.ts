0f7d254f396db5925c4849a254a1ce6107189bda
Christos Panagiotakopoulos <chrispanag@gmail.com>
2021-03-22 21:43:30 +0200

    [Refactor] Rename folder in viber-elements

---
+++ packages/viber-elements/lib/viberAPI/index.ts
@@ -0,0 +1,12 @@
+/**
+ * ebony-framework
+ *
+ * @module sendAPI
+ * @author George Rouvalis <georouvv@gmail.com>
+ * @copyright Copyright(c) 2021 Christos Panagiotakopoulos
+ * @license MIT
+ *
+ */
+
+export * from './attachments';
+export { Message } from './message';
