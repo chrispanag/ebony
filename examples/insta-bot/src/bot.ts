f7a5a4ed89c6a9c00b5ff83bd9ebcdc818e33140
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2021-07-14 17:45:56 +0300

    Fix the story mention example

--- examples/insta-bot/src/bot.ts
+++ examples/insta-bot/src/bot.ts
@@ -1,11 +1,9 @@
 import { Bot, InMemoryUser, inMemoryUserLoader } from '@ebenos/framework';
 import { fbConfig } from './secret';
 import { MessengerAdapter } from '@ebenos/messenger-adapter';
-import { Message } from '@ebenos/elements';
 
 import getStarted from './modules/getStarted';
-
-const DELAY = 10;
+import { story_mention } from './modules/getStarted/actions';
 
 export const adapter = new MessengerAdapter(fbConfig, inMemoryUserLoader());
 
@@ -14,36 +12,3 @@ export const bot = new Bot<InMemoryUser>(adapter, {});
 bot.addModule(getStarted);
 
 bot.addStoryMentionRule(story_mention);
-bot.addStoryMentionRule(story_mention1);
-async function story_mention(user: InMemoryUser) {
-    return bot
-        .scenario(user)
-        .send(
-            new Message({
-                text: `Hi there!!! 😄`
-            })
-        )
-        .wait(DELAY)
-        .send(
-            new Message({
-                text: 'Now I am on instagram too.'
-            })
-        )
-        .end();
-}
-async function story_mention1(user: InMemoryUser) {
-    return bot
-        .scenario(user)
-        .send(
-            new Message({
-                text: `Hi there!!! 😄`
-            })
-        )
-        .wait(DELAY)
-        .send(
-            new Message({
-                text: 'Now I am on instagram too.'
-            })
-        )
-        .end();
-}
