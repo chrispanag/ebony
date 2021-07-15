f6268c2d3a00692afd4fcbf101cb0e4a50644697
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2021-07-15 17:45:58 +0300

    Modify AttachmeRoute to extend basicRoute

--- packages/framework/lib/routers/AttachmentRouter.ts
+++ packages/framework/lib/routers/AttachmentRouter.ts
@@ -1,27 +1,3 @@
-interface IRoutes {
-    [key: string]: (...args: any[]) => any;
-}
+import BasicRouter from './BasicRouter';
 
-/**
- * A Basic Router Class
- */
-export default class AttachmentRouter {
-    private routes: IRoutes = {};
-    /**
-     * Adds routes to this router
-     */
-    public importRoutes(routes = {}): void {
-        this.routes = Object.assign(routes, this.routes);
-    }
-
-    /**
-     * Checks if the route exists and if yes returns the function. If it doesn't exist it returns false
-     */
-    public getRoute(route: string): ((...args: any[]) => any) | false {
-        if (route in this.routes) {
-            return this.routes[route];
-        }
-
-        return false;
-    }
-}
+export default class AttachmentRouter extends BasicRouter {}
