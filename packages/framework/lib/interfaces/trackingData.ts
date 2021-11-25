beb35094e05ded508adb4e80f396bbea32b25357
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2021-11-25 12:52:43 +0200

    Change tracking data type

--- packages/framework/lib/interfaces/trackingData.ts
+++ packages/framework/lib/interfaces/trackingData.ts
@@ -1,13 +1,5 @@
-type TrackingDataObj = Record<
-    string,
-    | number
-    | string
-    | boolean
-    | Array<TrackingDataObj | string | number | boolean>
-    | TrackingDataCopy
->;
-// this is realy a work around cause of the ts version
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface TrackingDataCopy extends TrackingDataObj {}
-
-export type ITrackingData = TrackingDataObj | string;
+export type TrackingDataPrimitives = null | string | number | boolean;
+export type ITrackingData =
+    | TrackingDataPrimitives
+    | Array<TrackingDataPrimitives | ITrackingData>
+    | { [key: string]: ITrackingData };
