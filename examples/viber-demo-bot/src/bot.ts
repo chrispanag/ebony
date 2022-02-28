0fd74478b00ab986e20c109cd8d3ecd5e84d6ec8
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2022-02-28 18:30:15 +0200

    Fix

--- examples/viber-demo-bot/src/bot.ts
+++ examples/viber-demo-bot/src/bot.ts
@@ -3,9 +3,25 @@ import { ViberAdapter } from '@ebenos/viber-adapter';
 import { viberConfig } from './secret';
 
 import getStartedModule from './modules/getStarted';
+import {
+    IViberConversationStartedEvent,
+    IViberSubscribedEvent,
+    IViberUnsubscribedEvent
+} from '@ebenos/viber-adapter';
 
 export const adapter = new ViberAdapter({
-    authToken: viberConfig.auth_token
+    authToken: viberConfig.auth_token,
+    webhookHanlers: {
+        conversationStartedWebhook: async (e: IViberConversationStartedEvent) => {
+            console.log(e);
+        },
+        subscribeWebhook: async (e: IViberSubscribedEvent) => {
+            console.log(e);
+        },
+        unsubscribeWebhook: async (e: IViberUnsubscribedEvent) => {
+            console.log(e);
+        }
+    }
 });
 
 export const bot = new Bot<InMemoryUser>(adapter, {});
--- packages/viber-adapter/lib/index.ts
+++ packages/viber-adapter/lib/index.ts
@@ -1,3 +1,8 @@
 import ViberAdapter from './adapter';
 
+export * from './interfaces/message_types';
+export * from './interfaces/tracking_data';
+export * from './interfaces/api';
+export * from './interfaces/webhook';
+export * from './adapter';
 export { ViberAdapter };
